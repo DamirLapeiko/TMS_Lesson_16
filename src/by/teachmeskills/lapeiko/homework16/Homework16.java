c85a3f5017fd172e415eb87ea08af3de0c879590
Damir Lapeiko <lapeiko02@maik.ru>
2023-03-08 14:11:11 +0300

    Creating realization for feature

---
+++ src/by/teachmeskills/lapeiko/homework16/Homework16.java
@@ -0,0 +1,58 @@
+package by.teachmeskills.lapeiko.homework16;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Homework16 {
+    private static ChatService chat;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Write number of messages: ");
+        int limitOfPosts = Integer.parseInt(scanner.nextLine());
+        System.out.print("Write delay time in seconds: ");
+        Duration delayTime = Duration.ofSeconds(Integer.parseInt(scanner.nextLine()));
+        System.out.println(
+                """
+                        Choose task:
+                        1. Enter chat
+                        2. Show chat history
+                        0. Exit"""
+        );
+        while (true) {
+            System.out.print("Task number: ");
+            int taskNumber = Integer.parseInt(scanner.nextLine());
+            switch (taskNumber) {
+                case 1 -> {
+                    chat = new ChatService(limitOfPosts, delayTime);
+                    int counter = 0;
+                    do {
+                        System.out.print("Enter nickname: ");
+                        User user = new User(scanner.nextLine());
+                        System.out.print("Enter message: ");
+                        String message = scanner.nextLine();
+                        if (chat.addNewMessage(user, message)) {
+                            System.out.println("Message added to history");
+                        } else {
+                            System.out.println("You used all tries to write a message. Wait 3 min");
+                        }
+                        if (counter++ == 10) {
+                            break;
+                        }
+                    } while (true);
+                }
+                case 2 -> {
+                    System.out.println("Chat history");
+                    System.out.println(Arrays.toString(chat.getHistoryOfMessages()));
+                }
+                case 0 -> {
+                    return;
+                }
+                default -> System.err.println("Task doesn't exist");
+            }
+            System.out.println();
+        }
+    }
+}
